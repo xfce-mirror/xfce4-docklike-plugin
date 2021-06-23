50b11d707f9d99049748c75e1648164ddfa9a45c
David Keogh <davidtkeogh@gmail.com>
2021-06-22 21:30:00 -0700

    Always remove hover state from the previous active group

--- src/Wnck.cpp
+++ src/Wnck.cpp
@@ -88,7 +88,10 @@ namespace Wnck
 					{
 						GroupWindow* prevWindow = mGroupWindows.get(prevXID);
 						if (prevWindow != NULL)
+						{
+							prevWindow->mGroup->mSHover = false;
 							Help::Gtk::cssClassRemove(GTK_WIDGET(prevWindow->mGroupMenuItem->mItem), "active_menu_item");
+						}
 					}
 				}
 				setActiveWindow();
