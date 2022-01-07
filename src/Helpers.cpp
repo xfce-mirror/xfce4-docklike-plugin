2441021fd004643b1a1a9ef1d2d175407b73644c
Hakan Erduman <hakan@erduman.de>
2022-01-07 18:22:43 +0100

    basename() called but <libgen.h> not included
    Seen while porting to alpine linux.
    This compiles on GNU libc since it is not posixly strict
    enough in regard to header files but fails on e.g. musl
    or other c libraries.
    
    The patch adresses this by using g_path_get_basename from
    glib instead. This may also help compilation on the *BSDs.
    and void linux, musl variety.
    
    Fixes: #33

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -83,7 +83,7 @@ namespace Help
 		{
 			char buffer[1024];
 			strcpy(buffer, str.c_str());
-			return basename(buffer);
+			return g_path_get_basename(buffer);
 		}
 
 		std::string trim(const std::string str)
