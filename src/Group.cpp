xfce-mirror/xfce4-docklike-plugin

commit dfa6d08ef506be32e7909ff71af9a8452e2cb12a

Add option to change behavior of middle mouse button (Closes #32)

src/Group.cpp
@@ -847,7 +847,19 @@ void Group::onButtonPress(GdkEventButton* event)
 void Group::onButtonRelease(GdkEventButton* event)
 {
 	if (event->button == GDK_BUTTON_MIDDLE)
-		closeAll();
+	{
+		switch(Settings::middleButtonBehavior)
+		{
+			case BEHAVIOR_CLOSE_ALL:
+			{
+				closeAll();
+			}
+			case BEHAVIOR_LAUNCH_NEW:
+			{
+				mAppInfo->launch();
+			}
+		}
+	}
 	else if (event->state & GDK_SHIFT_MASK || (mPinned && !mWindowsCount))
 		mAppInfo->launch();
 	else if (mActive)

src/Group.hpp
@@ -20,6 +20,12 @@
 #include "Helpers.hpp"
 #include "State.tpp"
 
+enum MiddleButtonBehavior
+{
+	BEHAVIOR_CLOSE_ALL,
+	BEHAVIOR_LAUNCH_NEW
+};
+
 enum IndicatorOrientation
 {
 	ORIENTATION_AUTOMATIC,

src/_dialogs.xml
@@ -168,6 +168,46 @@
                         <property name="top_attach">4</property>
                       </packing>
                     </child>
+                    <child>
+                      <object class="GtkBox">
+                        <property name="visible">True</property>
+                        <property name="can_focus">False</property>
+                        <property name="orientation">horizontal</property>
+                        <property name="spacing">6</property>
+                        <child>
+                          <object class="GtkLabel">
+                            <property name="visible">True</property>
+                            <property name="can_focus">False</property>
+                            <property name="halign">start</property>
+                            <property name="label" translatable="yes">Middle mouse button:</property>
+                          </object>
+                          <packing>
+                            <property name="expand">False</property>
+                            <property name="fill">True</property>
+                            <property name="position">0</property>
+                          </packing>
+                        </child>
+                        <child>
+                          <object class="GtkComboBoxText" id="co_middleButtonBehavior">
+                            <property name="visible">True</property>
+                            <property name="can_focus">False</property>
+                            <items>
+                              <item translatable="yes">Close all</item>
+                              <item translatable="yes">Launch new instance</item>
+                            </items>
+                          </object>
+                          <packing>
+                            <property name="expand">False</property>
+                            <property name="fill">True</property>
+                            <property name="position">1</property>
+                          </packing>
+                        </child>
+                      </object>
+                      <packing>
+                        <property name="left_attach">0</property>
+                        <property name="top_attach">5</property>
+                      </packing>
+                    </child>
                   </object>
                 </child>
                 <child type="label">

src/Settings.cpp
@@ -19,6 +19,7 @@ namespace Settings
 	State<bool> onlyDisplayVisible;
 	State<bool> onlyDisplayScreen;
 	State<bool> showPreviews;
+	State<int> middleButtonBehavior;
 
 	State<int> indicatorOrientation;
 	State<int> indicatorStyle;
@@ -67,6 +68,12 @@ namespace Settings
 				saveFile();
 
 				Dock::drawGroups();
+			});
+
+		middleButtonBehavior.setup(g_key_file_get_integer(mFile, "user", "middleButtonBehavior", NULL),
+			[](int middleButtonBehavior) -> void {
+				g_key_file_set_integer(mFile, "user", "middleButtonBehavior", middleButtonBehavior);
+				saveFile();
 			});
 
 		indicatorOrientation.setup(g_key_file_get_integer(mFile, "user", "indicatorOrientation", NULL),

src/Settings.hpp
@@ -38,6 +38,7 @@ namespace Settings
 	extern State<bool> onlyDisplayScreen;
 	extern State<bool> showPreviews;
 	extern State<bool> showWindowCount;
+	extern State<int> middleButtonBehavior;
 
 	extern State<int> indicatorOrientation;
 	extern State<int> indicatorStyle;

src/SettingsDialog.cpp
@@ -93,6 +93,14 @@ namespace SettingsDialog
 			}),
 			NULL);
 
+		GObject* middleButtonBehavior = gtk_builder_get_object(builder, "co_middleButtonBehavior");
+		gtk_combo_box_set_active(GTK_COMBO_BOX(middleButtonBehavior), Settings::middleButtonBehavior);
+		g_signal_connect(middleButtonBehavior, "changed",
+			G_CALLBACK(+[](GtkComboBox* middleButtonBehavior, GtkWidget* g) {
+				Settings::middleButtonBehavior.set(gtk_combo_box_get_active(GTK_COMBO_BOX(middleButtonBehavior)));
+			}),
+			dialog);
+
 		// =====================================================================
 
 		GObject* indicatorOrientation = gtk_builder_get_object(builder, "co_indicatorOrientation");

commit b82687b5272c6785b54c1fa5f2a1b220ac98d9ad

Added middle mouse button do nothing option
Fixes: dfa6d08ef506be32e7909ff71af9a8452e2cb12a
Closes: #58

src/Group.cpp
@@ -853,10 +853,16 @@ void Group::onButtonRelease(GdkEventButton* event)
 			case BEHAVIOR_CLOSE_ALL:
 			{
 				closeAll();
+				break;
 			}
 			case BEHAVIOR_LAUNCH_NEW:
 			{
 				mAppInfo->launch();
+				break;
+			}
+			case BEHAVIOR_DO_NOTHING:
+			{
+				break;
 			}
 		}
 	}

src/Group.hpp
@@ -23,7 +23,8 @@
 enum MiddleButtonBehavior
 {
 	BEHAVIOR_CLOSE_ALL,
-	BEHAVIOR_LAUNCH_NEW
+	BEHAVIOR_LAUNCH_NEW,
+	BEHAVIOR_DO_NOTHING
 };
 
 enum IndicatorOrientation

src/_dialogs.xml
@@ -194,6 +194,7 @@
                             <items>
                               <item translatable="yes">Close all</item>
                               <item translatable="yes">Launch new instance</item>
+                              <item translatable="yes">Do Nothing</item>
                             </items>
                           </object>
                           <packing>