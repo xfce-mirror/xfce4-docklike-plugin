xfce-mirror/xfce4-docklike-plugin

commit 8a9f6d7317c9d9d22a2a642fe47ed4b87edb6cbe

GroupMenu: Re-popup if windows are added/removed when shown
We don't actually have to rerun all the popup() code, but almost, and
it's simpler. It's necessary to defer this to avoid an X11 crash when
updating thumbnails, at least.

Fixes: #25

src/GroupMenu.cpp
@@ -73,6 +73,9 @@ GroupMenu::~GroupMenu()
 void GroupMenu::add(GroupMenuItem* menuItem)
 {
 	gtk_box_pack_end(GTK_BOX(mBox), GTK_WIDGET(menuItem->mItem), false, true, 0);
+
+	if (mVisible)
+		g_idle_add([](gpointer data) { ((GroupMenu*)data)->popup(); return FALSE; }, this);
 }
 
 void GroupMenu::remove(GroupMenuItem* menuItem)
@@ -82,6 +85,9 @@ void GroupMenu::remove(GroupMenuItem* menuItem)
 
 	if (mGroup->mWindowsCount < (Settings::noWindowsListIfSingle ? 2 : 1))
 		gtk_widget_hide(mWindow);
+
+	if (mVisible)
+		g_idle_add([](gpointer data) { ((GroupMenu*)data)->popup(); return FALSE; }, this);
 }
 
 void GroupMenu::popup()

commit 0ff4c50b41b7f0610cdefb11c48e685bb9ce86c3

Fix pending source when GroupMenu is destroyed
A regression introduced in 8a9f6d73 once again caused a crash when
reloading desktop files with the windows menu open. The addition of a
small Idle class copied from Timeout seems appropriate (although this
could probably all be the subject of a single class).

Fixes: 8a9f6d7317c9d9d22a2a642fe47ed4b87edb6cbe
Related: #25

src/GroupMenu.cpp
@@ -25,6 +25,11 @@ GroupMenu::GroupMenu(Group* dockButton)
 	gtk_container_add(GTK_CONTAINER(mWindow), mBox);
 	gtk_widget_show(mBox);
 
+	mPopupIdle.setup([this]() {
+		popup();
+		return false;
+	});
+
 	//--------------------------------------------------
 
 	g_signal_connect(G_OBJECT(mWindow), "enter-notify-event",
@@ -67,6 +72,7 @@ GroupMenu::GroupMenu(Group* dockButton)
 
 GroupMenu::~GroupMenu()
 {
+	mPopupIdle.stop();
 	gtk_widget_destroy(mWindow);
 }
 
@@ -75,7 +81,7 @@ void GroupMenu::add(GroupMenuItem* menuItem)
 	gtk_box_pack_end(GTK_BOX(mBox), GTK_WIDGET(menuItem->mItem), false, true, 0);
 
 	if (mVisible)
-		g_idle_add([](gpointer data) { ((GroupMenu*)data)->popup(); return FALSE; }, this);
+		mPopupIdle.start();
 }
 
 void GroupMenu::remove(GroupMenuItem* menuItem)
@@ -87,7 +93,7 @@ void GroupMenu::remove(GroupMenuItem* menuItem)
 		gtk_widget_hide(mWindow);
 
 	if (mVisible)
-		g_idle_add([](gpointer data) { ((GroupMenu*)data)->popup(); return FALSE; }, this);
+		mPopupIdle.start();
 }
 
 void GroupMenu::popup()

src/GroupMenu.hpp
@@ -12,6 +12,8 @@
 
 #include <iostream>
 
+#include "Helpers.hpp"
+
 class Group;
 class GroupMenuItem;
 
@@ -39,6 +41,8 @@ class GroupMenu
 
 	bool mVisible;
 	bool mMouseHover;
+
+	Help::Gtk::Idle mPopupIdle;
 };
 
 #endif // GROUP_MENU_HPP

src/Helpers.cpp
@@ -181,6 +181,37 @@ namespace Help
 				mTimeoutId = 0;
 			}
 		}
+
+		Idle::Idle()
+		{
+			mIdleId = 0;
+		}
+
+		void Idle::setup(std::function<bool()> function)
+		{
+			mFunction = function;
+		}
+
+		void Idle::start()
+		{
+			stop();
+			mIdleId = g_idle_add(G_SOURCE_FUNC(+[](Idle* me) {
+				bool cont = me->mFunction();
+				if (!cont)
+					me->mIdleId = 0;
+				return cont;
+			}),
+			this);
+		}
+
+		void Idle::stop()
+		{
+			if (mIdleId != 0)
+			{
+				g_source_remove(mIdleId);
+				mIdleId = 0;
+			}
+		}
 	} // namespace Gtk
 
 } // namespace Help

src/Helpers.hpp
@@ -56,6 +56,19 @@ namespace Help
 
 			uint mTimeoutId;
 		};
+
+		class Idle
+		{
+			public:
+			Idle();
+
+			void setup(std::function<bool()> function);
+			void start();
+			void stop();
+
+			std::function<bool()> mFunction;
+			uint mIdleId;
+		};
 	} // namespace Gtk
 } // namespace Help
 