7b535565382022daf576b4e3f2cec32932806a1f
Gaël Bonithon <gael@xfce.org>
2025-10-20 17:24:08 +0200

    GroupWindow: Add missing sanity check
    There are a few situations on wayland where this can happen (output
    changes, etc.)

--- src/GroupWindow.cpp
+++ src/GroupWindow.cpp
@@ -165,7 +165,8 @@ void GroupWindow::updateState()
 		GList* monitors = xfw_window_get_monitors(mXfwWindow);
 		GdkWindow* pluginWindow = gtk_widget_get_window(GTK_WIDGET(Plugin::mXfPlugin));
 
-		if (!g_list_find_custom(
+		if (pluginWindow == nullptr
+			|| !g_list_find_custom(
 				monitors,
 				gdk_display_get_monitor_at_window(Plugin::mDisplay, pluginWindow),
 				[](gconstpointer a, gconstpointer b) { return xfw_monitor_get_gdk_monitor((XfwMonitor*)a) == b ? 0 : 1; }))
