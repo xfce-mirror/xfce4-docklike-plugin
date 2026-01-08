xfce-mirror/xfce4-docklike-plugin

commit 9659ec485b3b19bc0a92f3cadff7a63b398e9341

Hide workspace-related visibility option on wayland
And minimalist management of workspace group change on wayland.

Related: xfce/libxfce4windowing#53

src/Xfw.cpp
@@ -29,7 +29,7 @@
 namespace Xfw
 {
 	XfwScreen* mXfwScreen;
-	XfwWorkspaceGroup* mXfwWorkspaceGroup;
+	XfwWorkspaceGroup* mXfwWorkspaceGroup = nullptr;
 	Store::KeyStore<XfwWindow*, std::shared_ptr<GroupWindow>> mGroupWindows;
 	std::unordered_set<std::string> mInvalidClassIds = {
 		"wine",
@@ -129,18 +129,28 @@ namespace Xfw
 			}),
 			nullptr);
 
-		XfwWorkspaceManager* wpManager = xfw_screen_get_workspace_manager(Xfw::mXfwScreen);
-		mXfwWorkspaceGroup = XFW_WORKSPACE_GROUP(xfw_workspace_manager_list_workspace_groups(wpManager)->data);
-		g_signal_connect(G_OBJECT(mXfwWorkspaceGroup), "active-workspace-changed",
-			G_CALLBACK(+[](XfwScreen* screen, XfwWindow* xfwWindow) {
-				setVisibleGroups();
-			}),
-			nullptr);
+		// window<->workspace association only works on X11, where there is only one workspace group,
+		// but it can be destroyed on wayland, so let's manage this in a minimalist way
+		XfwWorkspaceManager* wpManager = xfw_screen_get_workspace_manager(mXfwScreen);
+		auto workspaceGroupDestroyed = +[](XfwWorkspaceManager* manager, XfwWorkspaceGroup* group, gpointer data) {
+			if (group == mXfwWorkspaceGroup)
+			{
+				mXfwWorkspaceGroup = XFW_WORKSPACE_GROUP(xfw_workspace_manager_list_workspace_groups(manager)->data);
+				g_signal_connect(G_OBJECT(mXfwWorkspaceGroup), "active-workspace-changed",
+					G_CALLBACK(+[](XfwScreen* screen, XfwWindow* xfwWindow) {
+						setVisibleGroups();
+					}),
+					nullptr);
+			}
+		};
+		g_signal_connect(wpManager, "workspace-group-destroyed", G_CALLBACK(workspaceGroupDestroyed), nullptr);
+		workspaceGroupDestroyed(wpManager, nullptr, nullptr);
 	}
 
 	void finalize()
 	{
 		mGroupWindows.clear();
+		g_signal_handlers_disconnect_by_data(xfw_screen_get_workspace_manager(mXfwScreen), nullptr);
 		g_signal_handlers_disconnect_by_data(mXfwWorkspaceGroup, nullptr);
 		g_signal_handlers_disconnect_by_data(mXfwScreen, nullptr);
 		g_object_unref(mXfwScreen);

src/SettingsDialog.cpp
@@ -83,13 +83,23 @@ namespace SettingsDialog
 			nullptr);
 
 		GObject* onlyDisplayVisible = gtk_builder_get_object(builder, "c_onlyDisplayVisible");
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(onlyDisplayVisible), Settings::onlyDisplayVisible);
-		g_signal_connect(onlyDisplayVisible, "toggled",
-			G_CALLBACK(+[](GtkToggleButton* _onlyDisplayVisible) {
-				Settings::onlyDisplayVisible.set(gtk_toggle_button_get_active(_onlyDisplayVisible));
-				Xfw::setVisibleGroups();
-			}),
-			nullptr);
+#ifdef ENABLE_X11
+		if (GDK_IS_X11_DISPLAY(gdk_display_get_default()))
+		{
+			// window<->workspace association only works on X11
+			gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(onlyDisplayVisible), Settings::onlyDisplayVisible);
+			g_signal_connect(onlyDisplayVisible, "toggled",
+				G_CALLBACK(+[](GtkToggleButton* _onlyDisplayVisible) {
+					Settings::onlyDisplayVisible.set(gtk_toggle_button_get_active(_onlyDisplayVisible));
+					Xfw::setVisibleGroups();
+				}),
+				nullptr);
+		}
+		else
+#endif
+		{
+			gtk_widget_hide(GTK_WIDGET(onlyDisplayVisible));
+		}
 
 		GObject* onlyDisplayScreen = gtk_builder_get_object(builder, "c_onlyDisplayScreen");
 		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(onlyDisplayScreen), Settings::onlyDisplayScreen);

commit 4353faa553fbf7609de70927f81b9904776e5c6d

wayland: Handle null workspace group list when a group is destroyed
Fixes: 9659ec485b3b19bc0a92f3cadff7a63b398e9341
Related: xfce/xfce4-panel#963

src/Xfw.cpp
@@ -132,8 +132,8 @@ namespace Xfw
 		// window<->workspace association only works on X11, where there is only one workspace group,
 		// but it can be destroyed on wayland, so let's manage this in a minimalist way
 		XfwWorkspaceManager* wpManager = xfw_screen_get_workspace_manager(mXfwScreen);
-		auto workspaceGroupDestroyed = +[](XfwWorkspaceManager* manager, XfwWorkspaceGroup* group, gpointer data) {
-			if (group == mXfwWorkspaceGroup)
+		auto workspaceGroupCreated = +[](XfwWorkspaceManager* manager, XfwWorkspaceGroup* group, gpointer data) {
+			if (mXfwWorkspaceGroup == nullptr)
 			{
 				mXfwWorkspaceGroup = XFW_WORKSPACE_GROUP(xfw_workspace_manager_list_workspace_groups(manager)->data);
 				g_signal_connect(G_OBJECT(mXfwWorkspaceGroup), "active-workspace-changed",
@@ -143,6 +143,26 @@ namespace Xfw
 					nullptr);
 			}
 		};
+		auto workspaceGroupDestroyed = +[](XfwWorkspaceManager* manager, XfwWorkspaceGroup* group, gpointer data) {
+			if (group == mXfwWorkspaceGroup)
+			{
+				GList* groups = xfw_workspace_manager_list_workspace_groups(manager);
+				if (groups == nullptr)
+				{
+					mXfwWorkspaceGroup = nullptr;
+				}
+				else
+				{
+					mXfwWorkspaceGroup = XFW_WORKSPACE_GROUP(groups->data);
+					g_signal_connect(G_OBJECT(mXfwWorkspaceGroup), "active-workspace-changed",
+						G_CALLBACK(+[](XfwScreen* screen, XfwWindow* xfwWindow) {
+							setVisibleGroups();
+						}),
+						nullptr);
+				}
+			}
+		};
+		g_signal_connect(wpManager, "workspace-group-created", G_CALLBACK(workspaceGroupCreated), nullptr);
 		g_signal_connect(wpManager, "workspace-group-destroyed", G_CALLBACK(workspaceGroupDestroyed), nullptr);
 		workspaceGroupDestroyed(wpManager, nullptr, nullptr);
 	}