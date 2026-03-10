194d52cedad184cd7eeaf7a5a6ada0409ae70e65
Zetta1-Reid0 <11255-Zetta1_Reid0@users.noreply.gitlab.xfce.org>
2026-03-10 10:24:12 +0200

    fix binding

--- src/SettingsDialog.cpp
+++ src/SettingsDialog.cpp
@@ -240,7 +240,7 @@ namespace SettingsDialog
 
 		GObject* previewHeightButton = gtk_builder_get_object(builder, "previewHeightButton");
 		gtk_spin_button_set_value(GTK_SPIN_BUTTON(previewHeightButton), Settings::previewHeight);
-		g_object_bind_property(showPreviews, "active", previewHeightButton, "sensitive", G_BINDING_DEFAULT);
+		g_object_bind_property(showPreviews, "active", previewHeightButton, "sensitive", G_BINDING_SYNC_CREATE);
 		g_signal_connect(previewHeightButton, "value-changed",
 			G_CALLBACK(+[](GtkSpinButton* _previewHeightButton) {
 				Settings::previewHeight.set(gtk_spin_button_get_value_as_int(_previewHeightButton));
