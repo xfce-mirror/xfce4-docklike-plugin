679312f96080c39e986e4f8725c18939421084af
Zetta1-Reid0 <11255-Zetta1_Reid0@users.noreply.gitlab.xfce.org>
2026-03-11 18:27:31 +0200

    Final fix

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -84,8 +84,8 @@ namespace Settings
 				});
 			});
 
-  int previewWidth = g_key_file_get_integer(file, "user", "previewWidth", nullptr);
-		previewWidth.setup(previewWidth > 0 ? previewWidth : defPreviewWidth,
+		int pWidth = g_key_file_get_integer (file, "user", "previewWidth", nullptr);
+		previewWidth.setup(pWidth > 0 ? pWidth : defPreviewWidth,
 			[](int _previewWidth) -> void {
 				if (_previewWidth <= 0)
 				{
@@ -101,9 +101,10 @@ namespace Settings
 				});
 			});
 
-		previewHeight.setup(g_key_file_get_integer(file, "user", "previewHeight", nullptr),
+		int pHeight = g_key_file_get_integer (file, "user", "previewHeight", nullptr);
+		previewHeight.setup(pHeight > 0 ? pHeight : defPreviewHeight,
 			[](int _previewHeight) -> void {
-				if (_previewHeight == 0)
+				if (_previewHeight <= 0)
 				{
 					previewHeight.set(defPreviewHeight);
 					return;
