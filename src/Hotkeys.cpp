7602e4eff3388407edbc136523b7ca5b859c0894
Ronit Ramdam BK <ron.ram5126@gmail.com>
2026-03-17 22:52:43 +0545

    fix: correct license text formatting in Hotkeys.cpp

--- src/Hotkeys.cpp
+++ src/Hotkeys.cpp
@@ -4,8 +4,7 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the Li			unsigned int keycode = XKeysymToKeycode(xdisplay, keyval);
-			if (!keycode)nse, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
